e889723260e72f45ce0c3660807a24fd52547f07
elly <eleanorz@gmail.com>
2014-04-21 13:18:21 -0700

    additional changes made to play around with input from user adding integers to the array for checking

--- even_numbers.py
+++ even_numbers.py
@@ -1,6 +1,10 @@
 # This program extracts even numbers from an input array of other numbers
 
+number = input("What number do you want to check?  ")
+
+
 numbers = [1,2,3,4,5,6,7]
+numbers.append(number)
 
 def even_number(numbers):
 	print numbers
