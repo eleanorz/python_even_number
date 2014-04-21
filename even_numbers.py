89ce7ddca8d11508b78d000988139c510bf5bc83
elly <eleanorz@gmail.com>
2014-04-21 13:58:37 -0700

    conditionals added for different types

--- even_numbers.py
+++ even_numbers.py
@@ -1,19 +1,31 @@
 # This program extracts even numbers from an input array of other numbers
 
-number = input("What number do you want to check?  ")
+first_user_input = input("What number do you want to check?  ")
 
+print type(first_user_input)
+# if type(number) == string:
+# 	pass "i am string"
 
 numbers = [1,2,3,4,5,6,7]
-numbers.append(number)
+
+if type(first_user_input) == int:
+	first_user_input.append(number)
+ 	print "i am int"
+elif type(first_user_input) == list:
+	print "i am a list"
+	print first_user_input
+	numbers += first_user_input
+
+
+
 
 def even_number(numbers):
 	print numbers
 	for number in numbers:
-		if number%2 == 0:
-			print number
+		if type(number) == int:
+			if number%2 == 0:
+				 print number
 
 even_number(numbers)
 
-raw_input("")
-
-print 'hello world'
+# raw_input("exit")
