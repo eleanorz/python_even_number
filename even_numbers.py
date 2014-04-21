59dc615c36b8a10f1c918c4446852722575e8ced
elly <eleanorz@gmail.com>
2014-04-21 14:10:19 -0700

    last commit

--- even_numbers.py
+++ even_numbers.py
@@ -1,23 +1,19 @@
 # This program extracts even numbers from an input array of other numbers
 
-first_user_input = input("What number do you want to check?  ")
+first_user_input = input("What number or array do you want to check?  ")
 
 print type(first_user_input)
-# if type(number) == string:
-# 	pass "i am string"
 
 numbers = [1,2,3,4,5,6,7]
 
 if type(first_user_input) == int:
-	first_user_input.append(number)
+	numbers.append(first_user_input)
+
  	print "i am int"
 elif type(first_user_input) == list:
 	print "i am a list"
 	print first_user_input
 	numbers += first_user_input
-
-
-
 
 def even_number(numbers):
 	print numbers
@@ -27,5 +23,3 @@ def even_number(numbers):
 				 print number
 
 even_number(numbers)
-
-# raw_input("exit")
