364f20e0ccd803c112da8786ca05c6f9cd3df82a
elly <eleanorz@gmail.com>
2014-04-21 13:01:42 -0700

    second commit, is correctly parsing even numbers out

--- even_numbers.py
+++ even_numbers.py
@@ -1,3 +1,15 @@
 # This program extracts even numbers from an input array of other numbers
 
 numbers = [1,2,3,4,5,6,7]
+
+def even_number(numbers):
+	print numbers
+	for number in numbers:
+		if number%2 == 0:
+			print number
+
+even_number(numbers)
+
+raw_input("")
+
+print 'hello world'
